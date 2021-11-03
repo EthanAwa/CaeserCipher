f582dac1890e68affe869544558ba68a5bbc2e0e
EthanAwa <sherri.ethan@awatapu.school.nz>
2021-11-04 10:18:19 +1300

    added base component as well as the function to make sure that a message contains only letters

---
+++ base_component.py
@@ -0,0 +1,22 @@
+# Imports
+
+# Functions
+
+# Make sure that input has no numbers
+def string_input(question):
+    valid = False
+
+    while not valid:
+        answer = input(question)
+
+        # If the input contains a number, print error
+        if not answer.isalpha():
+            print("Please enter letters only, numbers are not allowed.")
+            valid = False
+
+        # If the input has no numbers, return input
+        if answer.isalpha():
+            valid = True
+
+    return answer
+
