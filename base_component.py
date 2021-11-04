bfbb682612913d757e6d26cd6ce3e3cbb24f7485
EthanAwa <sherri.ethan@awatapu.school.nz>
2021-11-05 11:58:42 +1300

    check that user input is only an integer

--- base_component.py
+++ base_component.py
@@ -27,3 +27,24 @@ def split_message(word):
     return list(word)
 
 
+# Make sure that input is integer only
+def int_check(question):
+    error = "Please enter a whole number that is more than 0 (1, 2, 3...)\n"
+
+    valid = False
+    while not valid:
+
+        try:
+            response = int(input(question))
+
+            if response <= 0:
+                print(error)
+
+            else:
+                return response
+
+        except ValueError:
+            print("Please enter a whole number only, not a decimal (e.g 3.0) "
+                  "or a word (e.g Three)\n")
+
+
